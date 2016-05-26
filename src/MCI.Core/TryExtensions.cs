7c3d5c404b1b4bcfe6190c541886abf7a3d44a79
稲畑 稔秋 <inahata@miharu.co.jp>
2016-05-26 21:26:40 +0900

    クリーニング

--- src/MCI.Core/TryExtensions.cs
+++ src/MCI.Core/TryExtensions.cs
@@ -9,37 +9,6 @@ namespace Miharu
 
     public static class TryExtensions
     {
-        /*
-        public static Try<B> Select<A, B>(this Try<A> source, Func<A, B> f)
-        {
-            if (source.IsSuccess)
-            {
-                return new Success<B>(f(source.Get()));
-            }
-            else
-            {
-                return new Failure<B>(source.GetException());
-            }
-        }
-
-
-
-        public static Try<C> SelectMany<A, B, C>(this Try<A> source, Func<A, Try<B>> f, Func<A, B, C> g)
-        {
-            if (source.IsSuccess)
-            {
-                var x = source.Get();
-
-                return f(x).FlatMap(y => new Success<C>(g(x, y)));
-            }
-            else
-            {
-                return new Failure<C>(source.GetException());
-            }
-        }
-        */
-
-
         public static Try<A> Where<A>(this Try<A> source, Func<A, bool> f)
         {
             if (source.IsSuccess)
