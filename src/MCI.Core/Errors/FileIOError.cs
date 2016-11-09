29f4ee74095dd8dfd30ea9dc047ade7f6721f17b
Toshiaki Inahata <darwin49@gmail.com>
2016-11-09 21:10:38 +0900

    IError の試験導入

---
+++ src/MCI.Core/Errors/FileIOError.cs
@@ -0,0 +1,21 @@
+﻿namespace Miharu.Errors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public interface INotEnoughDiskSpaceError : IFileIOError
+    {
+    }
+
+    public interface IFileNotFoundError : IFileIOError
+    {
+    }
+
+    public interface IDirectoryNotFoundError : IFileIOError
+    {
+    }
+}
