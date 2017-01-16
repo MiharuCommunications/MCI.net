80a253c739d324b9aa781926943f83e51a0c702f
Toshiaki Inahata <inahata@miharu.co.jp>
2017-01-16 10:02:37 +0900

    IDisposable 用の Helper メソッドの追加

---
+++ src/MCI.Core/Disposable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miharu
+{
+    public class Disposable : IDisposable
+    {
+        private bool disposed;
+
+        private Action callback;
+
+        public Disposable(Action callback)
+        {
+            this.disposed = false;
+            this.callback = callback;
+        }
+
+
+        private void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            this.disposed = true;
+
+            if (disposing)
+            {
+                try
+                {
+                    this.callback();
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    this.callback = null;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
