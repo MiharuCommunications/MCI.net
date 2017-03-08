5d8f22afd60939e87101952e9a8caf3c46ae324b
Toshiaki Inahata <inahata@miharu.co.jp>
2017-03-08 10:50:22 +0900

    :heavy_plus_sign: Unit の追加
    .net Framework に既に System.Void があるが、C# からは使えないため、`Unit` という名前にした。

---
+++ src/MCI.Core/Unit.cs
@@ -0,0 +1,22 @@
+﻿namespace Miharu
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public sealed class Unit
+    {
+        private Unit()
+        {
+        }
+
+        public static readonly Unit Instance;
+
+        static Unit()
+        {
+            Instance = new Unit();
+        }
+    }
+}
