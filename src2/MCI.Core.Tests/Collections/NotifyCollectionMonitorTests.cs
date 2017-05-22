89cbb8053ad44807f1e89ba81a99bb4748c6aa06
Toshiaki Inahata <inahata@miharu.co.jp>
2017-05-22 20:03:29 +0900

    fix vs2017 project

---
+++ src2/MCI.Core.Tests/Collections/NotifyCollectionMonitorTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miharu.Core.Tests.Collections
+{
+    using Xunit;
+    using Miharu.Collections;
+
+    public class NotifyCollectionMonitorTests
+    {
+    }
+}
