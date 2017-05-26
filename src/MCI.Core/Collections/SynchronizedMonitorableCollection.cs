e88bb4bb86f96321487d27758df17814a8f45b41
Toshiaki Inahata <inahata@miharu.co.jp>
2017-05-26 21:42:03 +0900

    SynchronizedMonitorableCollection を Obsolete に
    未実装のため

--- src/MCI.Core/Collections/SynchronizedMonitorableCollection.cs
+++ src/MCI.Core/Collections/SynchronizedMonitorableCollection.cs
@@ -9,6 +9,7 @@
     using System.Text;
     using System.Threading;
 
+    [Obsolete("未実装")]
     public class SynchronizedMonitorableCollection<T> : IList<T>, ICollection, INotifyCollectionChanged
     {
         private ReaderWriterLockSlim locker;
